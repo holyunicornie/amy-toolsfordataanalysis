fb307c345a35eabdaa4f5d60eeb33ec8f15b4df0
Amy Ngan Ngo <56796410+holyunicornie@users.noreply.github.com>
2021-01-21 21:26:38 +0700

    Update bokeh_project.py

--- bokeh_project.py
+++ bokeh_project.py
@@ -7,12 +7,16 @@
 get_ipython().system('pip install bokeh')
 import pandas as pd
 import numpy as np
-from bokeh.plotting import figure, show
+from bokeh.plotting import ColumnDataSource, figure, output_file, show
 from bokeh.io import output_notebook
 from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.models.widgets import Tabs, Panel
 from bokeh.models import CustomJS
-from ipywidgets import widgets
+from bokeh.transform import linear_cmap
+from bokeh.palettes import cividis
+from bokeh.layouts import column, row
+from bokeh.models import CustomJS, Slider
+
 healthEx = pd.read_csv('https://raw.githubusercontent.com/holyunicornie/amy-toolsfordataanalysis/main/healh-ex-per-capita-2010-2018.csv')
 lifeExpectancy = pd.read_csv('https://raw.githubusercontent.com/holyunicornie/amy-toolsfordataanalysis/main/life-expectancy-2010-2018.csv')
 population = pd.read_csv('https://raw.githubusercontent.com/holyunicornie/amy-toolsfordataanalysis/main/population-2010-2018-oecd.csv')
@@ -31,10 +35,17 @@ p.xaxis.axis_label = 'health ex per capita'
 p.yaxis.axis_label = 'life expectancy'
 p = figure(plot_width=600, plot_height=450, title = "OECD Countries from 2010-2018",toolbar_location=None,
           tools="hover", tooltips="@Country: @pop")
-p.scatter('lifeX','healthExp', source=df, fill_alpha=0.6, size=10)
-p.xaxis.axis_label = 'life expectancy'
-p.yaxis.axis_label = 'health expense'
-show(p)
+p.scatter('healthExp','lifeX', source=df, fill_alpha=0.6, size=10)
+healthX_slider = Slider(start=0, end=10000, value=1, step=1000, title="Health Expense")
+lifeX_slider = Slider(start=0, end=100, value=1, step=1, title="Life Expectancy")
+pop_slider = Slider(start=0, end=1000000000, value=1, step=10000, title="Population")
+year_slider = Slider(start=2010, end = 2018, value =1, step = 1, title="Year")
+layout = row(
+    p,
+    column(healthX_slider, lifeX_slider, pop_slider, year_slider),
+)
+output_file("slider.html", title="slider.py example")
+show(layout)
 
 
 # In[16]:
